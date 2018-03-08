b937c73dcf1d161ca02fa77dc0f5e2be95dd5d7f
Wei Li <36608740+levisjtu@users.noreply.github.com>
2018-03-08 12:53:00 -0600

    Create 1009.cpp

---
+++ 3.8/1009.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+	int L1,L2;
+	int i,j;
+	string string1;
+    getline(cin,string1);
+	L1=string1.length();
+	L2=L1;
+	for(i=L1-1;i>=0;i--)
+	{
+		if(string1[i]==' ')
+		{
+			for(j=i+1;j<L2;j++)
+			{
+				cout<<string1[j];
+			}
+			cout<<" ";
+			L2=i;
+		}
+		if(i==0)
+		{
+			for(j=0;j<L2;j++)
+			{
+				cout<<string1[j];
+			}
+		}
+	}
+
+	
+	
+	return 0;
+}
